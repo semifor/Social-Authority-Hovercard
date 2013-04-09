f3f7b610efcd1b9616ba267f02e545f208b32acc
Peter Bray <petertheyeti@gmail.com>
2013-04-09 11:13:58 -0700

    bare minimum extension

---
+++ TwitterUsers.js
@@ -0,0 +1,16 @@
+var TwitterUsers || {};
+
+TwitterUsers = 
+{
+  hover_delay = 300,
+
+  init : function() {
+    $('a[href*="twitter.com/"]').each(TwitterUsers.hookEachUser);
+  }
+
+  hookEachUser : function(index, el) {
+    alert(el);
+  }
+}
+
+TwitterUsers.init();
