70d94ac82bb8356692d24d6b96f500d196f4048e
Peter Bray <petertheyeti@gmail.com>
2013-04-09 15:08:42 -0700

    Beginning stuff.

--- TwitterUsers.js
+++ TwitterUsers.js
@@ -1,14 +1,30 @@
-var TwitterUsers = TwitterUsers || {};
-
-TwitterUsers = 
+/*
+ * A Chrome extension that does things on mouseover of Twitter links.
+ */
+function TwitterUsers()
 {
-  init : function() {
-    $('a[href*=/https?:\/\/(www\.)?twitter.com\//]').each(TwitterUsers.hookEachUser);
-  },
 
-  hookEachUser : function(index, el) {
-    alert(el);
+  function init() {
+
+    // Clear out old event namespace.
+    $(document).off('.metrics');
+
+    // Find all Twitter links and pass them to a handler.
+    $('a').filter(
+      function() {
+        return $(this).attr('href').match(/https?:\/\/(www\.)?twitter.com\//);
+      }
+    ).each(handleEachUser);
   }
+
+  function handleEachUser() {
+    $(this).css("background-color", "#ede7d0");}
+    $(this).on("mouseover.metrics", function() {});
+  }
+
+  init();
+
 }
 
-TwitterUsers.init();
+new TwitterUsers();
+
